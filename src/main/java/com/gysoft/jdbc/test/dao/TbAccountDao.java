ccf050e1dd3b44231da799ed5852c16132d897f0
zhouning <2267431887@qq.com>
2019-05-16 09:09:28 +0800

    调整SQL的insert使用

---
+++ src/main/java/com/gysoft/jdbc/test/dao/TbAccountDao.java
@@ -0,0 +1,11 @@
+package com.gysoft.jdbc.test.dao;
+
+import com.gysoft.jdbc.dao.EntityDao;
+import com.gysoft.jdbc.test.pojo.TbAccount;
+
+/**
+ * @author 周宁
+ * @Date 2019-05-16 8:59
+ */
+public interface TbAccountDao extends EntityDao<TbAccount,Integer> {
+}
---
+++ src/main/java/com/gysoft/jdbc/test/dao/TbAccountDaoImpl.java
@@ -0,0 +1,11 @@
+package com.gysoft.jdbc.test.dao;
+
+import com.gysoft.jdbc.dao.EntityDaoImpl;
+import com.gysoft.jdbc.test.pojo.TbAccount;
+
+/**
+ * @author 周宁
+ * @Date 2019-05-16 9:00
+ */
+public class TbAccountDaoImpl extends EntityDaoImpl<TbAccount,Integer> implements TbAccountDao{
+}
